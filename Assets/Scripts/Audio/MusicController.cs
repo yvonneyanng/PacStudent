3a317653cc774c62dec2b039d4395a45ff3a66b1
Yvonne Yang <yvnyang@icloud.com>
2025-10-25 16:48:54 +1100

    cleanup

--- Assets/Scripts/Audio/MusicController.cs
+++ Assets/Scripts/Audio/MusicController.cs
@@ -1,24 +1,21 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicController : MonoBehaviour
 {
-    
     public AudioSource source;
     public AudioClip introBGM;
     public AudioClip normalBGM;
     public AudioClip scaredBGM;
     public AudioClip ghostDeadBGM;
-    
+
+    bool isScaredPlaying = false;
+
     void Awake()
     {
-        if (source == null)
-        {
-            source = GetComponent<AudioSource>();
-        }
+        if (source == null) source = GetComponent<AudioSource>();
     }
-    
+
     void Start()
     {
         PlayIntroThenNormal();
@@ -26,19 +23,42 @@ public class MusicController : MonoBehaviour
 
     void Update()
     {
-        
+        // check if GameManager exists and adjust based on scaredTimer
+        if (GameManager.Instance)
+        {
+            bool shouldBeScared = GameManager.Instance.scaredTimer > 0f;
+            if (shouldBeScared && !isScaredPlaying)
+                PlayScared();
+            else if (!shouldBeScared && isScaredPlaying)
+                PlayNormal();
+        }
     }
-    
+
     public void PlayIntroThenNormal()
     {
         StopAllCoroutines();
         StartCoroutine(CoIntroThenLoop(normalBGM));
+        isScaredPlaying = false;
     }
-    
-    public void PlayScared() => Loop(scaredBGM);
-    public void PlayGhostDead() => Loop(ghostDeadBGM);
-    public void PlayNormal() => Loop(normalBGM);
-    
+
+    public void PlayScared()
+    {
+        Loop(scaredBGM);
+        isScaredPlaying = true;
+    }
+
+    public void PlayGhostDead()
+    {
+        Loop(ghostDeadBGM);
+        isScaredPlaying = false;
+    }
+
+    public void PlayNormal()
+    {
+        Loop(normalBGM);
+        isScaredPlaying = false;
+    }
+
     IEnumerator CoIntroThenLoop(AudioClip loopClip)
     {
         if (introBGM != null)
